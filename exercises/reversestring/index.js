af3d021e6e1b9561042faf0ff883a3155a5efbaf
Cory Bass <cory.bass.07@gmail.com>
2021-05-18 00:17:33 -0500

    added more variations of reversestring challenge

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -11,8 +11,31 @@
 // Call reverse on array
 // Join array into a string
 
-function reverse(str) {
-    return str.split("").reverse().join("");
-}
+// function reverse(str) {
+//     return str.split("").reverse().join("");
+// }
+
+// function reverse(str) {
+//     str = str.split("");
+//     let strArray = [];
+//     for (let i = 0; i < str.length; i++) {
+//         strArray.unshift(str[i]);
+//     }
+//     return strArray.join("");
+// }
+
+// function reverse(str) {
+//     let reversed = "";
+
+//     for (let character of str) {
+//         reversed = character + reversed;
+//     }
+
+//     return reversed;
+// }
+
+const reverse = (str) => str.split("").reduce((rev, char) => char + rev, "");
+
+console.log(reverse("apple"));
 
 module.exports = reverse;
