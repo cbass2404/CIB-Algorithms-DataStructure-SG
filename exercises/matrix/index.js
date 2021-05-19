12148c05e250105a48d365e4cffd64597dac3696
Cory Bass <cory.bass.07@gmail.com>
2021-05-18 23:58:19 -0500

    matrix solution added and practiced

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,91 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
+// function matrix(n) {
+//     const results = [];
+
+//     for (let i = 0; i < n; i++) {
+//         results.push([]);
+//     }
+
+//     let counter = 1;
+
+//     let startColumn = 0;
+//     let endColumn = n - 1;
+
+//     let startRow = 0;
+//     let endRow = n - 1;
+
+//     while (startColumn <= endColumn && startRow <= endRow) {
+//         for (let i = startColumn; i <= endColumn; i++) {
+//             results[startRow][i] = counter;
+//             counter++;
+//         }
+//         startRow++;
+
+//         for (let i = startRow; i <= endRow; i++) {
+//             results[i][endColumn] = counter;
+//             counter++;
+//         }
+//         endColumn--;
+
+//         for (let i = endColumn; i >= startColumn; i--) {
+//             results[endRow][i] = counter;
+//             counter++;
+//         }
+//         endRow--;
+
+//         for (let i = endRow; i >= startRow; i--) {
+//             results[i][startColumn] = counter;
+//             counter++;
+//         }
+//         startColumn++;
+//     }
+
+//     return results;
+// }
+
+// function matrix(n) {
+//     const results = [];
+//     let counter = 1;
+
+//     let startRow = 0;
+//     let endRow = n - 1;
+//     let startColumn = 0;
+//     let endColumn = n - 1;
+
+//     for (let i = 0; i < n; i++) {
+//         results.push([]);
+//     }
+
+//     while (startColumn <= endColumn && startRow <= endRow) {
+//         for (let i = startColumn; i <= endRow; i++) {
+//             results[startRow][i] = counter;
+//             counter++;
+//         }
+//         startRow++;
+
+//         for (let i = startRow; i <= endRow; i++) {
+//             results[i][endColumn] = counter;
+//             counter++;
+//         }
+//         endColumn--;
+
+//         for (let i = endColumn; i >= startColumn; i--) {
+//             results[endRow][i] = counter;
+//             counter++;
+//         }
+//         endRow--;
+
+//         for (let i = endRow; i >= startRow; i--) {
+//             results[i][startColumn] = counter;
+//             counter++;
+//         }
+//         startColumn++;
+//     }
+//     return results;
+// }
+
 function matrix(n) {
     const results = [];
 
@@ -23,10 +108,8 @@ function matrix(n) {
     }
 
     let counter = 1;
-
     let startColumn = 0;
     let endColumn = n - 1;
-
     let startRow = 0;
     let endRow = n - 1;
 
@@ -55,7 +138,6 @@ function matrix(n) {
         }
         startColumn++;
     }
-
     return results;
 }
 
