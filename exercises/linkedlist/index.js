18a573d18aca03bc47475f142ef1c2fd4988450f
Cory Bass <cory.bass.07@gmail.com>
2021-05-24 12:06:40 -0500

    added removeAt method to linkedlist

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -105,6 +105,26 @@ class LinkedList {
         }
         return null;
     }
+
+    removeAt(index) {
+        if (!this.head) {
+            return;
+        }
+
+        if (index === 0) {
+            this.head = this.head.next;
+            return;
+        }
+
+        const prev = this.getAt(index - 1);
+        if (!prev || !prev.next) {
+            return;
+        }
+
+        prev.next = prev.next.next;
+    }
+
+    insertAt(index, node) {}
 }
 
 module.exports = { Node, LinkedList };
